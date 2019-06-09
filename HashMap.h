b765cf929f0c250496ebfebfaac74601377918a4
Zeta <dany.hoffmang@live.com>
2019-06-09 00:16:26 -0600

    correction to the bad return HashTable empty with nullptr
    Only one line corrected when the previous version says return NULL but is invalid, now is return T ()

--- HashMap.h
+++ HashMap.h
@@ -6,42 +6,51 @@
 using namespace std;
 const long TABLE_SIZE = 12725525;
 template<class T>
-class HashMap 
+class HashMap
 {
 private:
-    HashEntry<T> **table;
+	HashEntry<T> **table;
 public:
-    HashMap() 
-    {
-        table = new HashEntry<T>*[TABLE_SIZE];
-        for (int i = 0; i < TABLE_SIZE; i++) { table[i] = NULL; }
-    }
-    T get(int &key) 
-    {
-        int hash = (key % TABLE_SIZE);
-        while (table[hash] != NULL && table[hash]->getKey() != key)
-        { hash = (hash + 1) % TABLE_SIZE; }
-        
-        if (table[hash] == NULL) { return NULL; }
-        else
-        { return table[hash]->getValue(); }
-    }
-    void put(int key, T value) 
-    {
-        int hash = (key % TABLE_SIZE);
-        while (table[hash] != NULL && table[hash]->getKey() != key)
-            {hash = (hash + 1) % TABLE_SIZE;}
-        if (table[hash] != NULL)
-            { delete table[hash]; }
-        table[hash] = new HashEntry<T>(key, value);
-    }
-    ~HashMap() 
-    {
-        for (int i = 0; i < TABLE_SIZE; i++)
-        { 
-            if (table[i] != NULL) delete table[i];
-        }
-        delete[] table;
-    }
+	HashMap()
+	{
+		table = new HashEntry<T>*[TABLE_SIZE];
+		for (int i = 0; i < TABLE_SIZE; i++) { table[i] = NULL; }
+	}
+	T get(int &key)
+	{
+		int hash = (key % TABLE_SIZE);
+		
+		while (table[hash] != NULL && table[hash]->getKey() != key)
+		{
+			hash = (hash + 1) % TABLE_SIZE;
+		}
+		
+		if (table[hash] == NULL) { return T(); }
+		else
+		{
+			return table[hash]->getValue();
+		}
+	}
+	void put(int key, T value)
+	{
+		int hash = (key % TABLE_SIZE);
+		while (table[hash] != NULL && table[hash]->getKey() != key)
+		{
+			hash = (hash + 1) % TABLE_SIZE;
+		}
+		if (table[hash] != NULL)
+		{
+			delete table[hash];
+		}
+		table[hash] = new HashEntry<T>(key, value);
+	}
+	~HashMap()
+	{
+		for (long i = 0; i < TABLE_SIZE; i++)
+		{
+			if (table[i] != NULL) delete table[i];
+		}
+		delete[] table;
+	}
 };
 #endif //!HASHMAP_H
